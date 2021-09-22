4de869a0f5a6fdb39609e38dcaf4c2b376aa8f74
Mohammad Javad Ahmadi <36800130+MJAHMADEE@users.noreply.github.com>
2021-09-22 14:43:29 +0330

    Create AFCID Graphical Interface for Automatic Annotation of New Images.html

---
+++ AFCID Graphical Interface for Automatic Annotation of New Images/AFCID Graphical Interface for Automatic Annotation of New Images.js
@@ -0,0 +1,197 @@
+$(function() {
+	//values pulled from query string
+	$('#model').val("capsulorhexis_sir");
+	$('#version').val("5");
+	$('#api_key').val("khtZm6G1rK8HpXWunFc4");
+
+	setupButtonListeners();
+});
+
+var infer = function() {
+	$('#output').html("Inferring...");
+	$("#resultContainer").show();
+	$('html').scrollTop(100000);
+
+	getSettingsFromForm(function(settings) {
+		settings.error = function(xhr) {
+			$('#output').html("").append([
+				"Error loading response.",
+				"",
+				"Check your API key, model, version,",
+				"and other parameters",
+				"then try again."
+			].join("\n"));
+		};
+
+		$.ajax(settings).then(function(response) {
+			if(settings.format == "json") {
+				var pretty = $('<pre>');
+				var formatted = JSON.stringify(response, null, 4)
+
+				pretty.html(formatted);
+				$('#output').html("").append(pretty);
+				$('html').scrollTop(100000);
+			} else {
+				var arrayBufferView = new Uint8Array(response);
+				var blob = new Blob([arrayBufferView], {
+					'type': 'image\/jpeg'
+				});
+				var base64image = window.URL.createObjectURL(blob);
+
+				var img = $('<img/>');
+				img.get(0).onload = function() {
+					$('html').scrollTop(100000);
+				};
+				img.attr('src', base64image);
+				$('#output').html("").append(img);
+			}
+		});
+	});
+};
+
+var retrieveDefaultValuesFromLocalStorage = function() {
+	try {
+		var api_key = localStorage.getItem("rf.api_key");
+		var model = localStorage.getItem("rf.model");
+		var format = localStorage.getItem("rf.format");
+
+		if (api_key) $('#api_key').val(api_key);
+		if (model) $('#model').val(model);
+		if (format) $('#format').val(format);
+	} catch (e) {
+		// localStorage disabled
+	}
+
+	$('#model').change(function() {
+		localStorage.setItem('rf.model', $(this).val());
+	});
+
+	$('#api_key').change(function() {
+		localStorage.setItem('rf.api_key', $(this).val());
+	});
+
+	$('#format').change(function() {
+		localStorage.setItem('rf.format', $(this).val());
+	});
+};
+
+var setupButtonListeners = function() {
+	// run inference when the form is submitted
+	$('#inputForm').submit(function() {
+		infer();
+		return false;
+	});
+
+	// make the buttons blue when clicked
+	// and show the proper "Select file" or "Enter url" state
+	$('.bttn').click(function() {
+		$(this).parent().find('.bttn').removeClass('active');
+		$(this).addClass('active');
+
+		if($('#computerButton').hasClass('active')) {
+			$('#fileSelectionContainer').show();
+			$('#urlContainer').hide();
+		} else {
+			$('#fileSelectionContainer').hide();
+			$('#urlContainer').show();
+		}
+
+		if($('#jsonButton').hasClass('active')) {
+			$('#imageOptions').hide();
+		} else {
+			$('#imageOptions').show();
+		}
+
+		return false;
+	});
+
+	// wire styled button to hidden file input
+	$('#fileMock').click(function() {
+		$('#file').click();
+	});
+
+	// grab the filename when a file is selected
+	$("#file").change(function() {
+		var path = $(this).val().replace(/\\/g, "/");
+		var parts = path.split("/");
+		var filename = parts.pop();
+		$('#fileName').val(filename);
+	});
+};
+
+var getSettingsFromForm = function(cb) {
+	var settings = {
+		method: "POST",
+	};
+
+	var parts = [
+		"https://detect.roboflow.com/",
+		$('#model').val(),
+		"/",
+		$('#version').val(),
+		"?api_key=" + $('#api_key').val()
+	];
+
+	var classes = $('#classes').val();
+	if(classes) parts.push("&classes=" + classes);
+
+	var confidence = $('#confidence').val();
+	if(confidence) parts.push("&confidence=" + confidence);
+
+	var overlap = $('#overlap').val();
+	if(overlap) parts.push("&overlap=" + overlap);
+
+	var format = $('#format .active').attr('data-value');
+	parts.push("&format=" + format);
+	settings.format = format;
+
+	if(format == "image") {
+		var labels = $('#labels .active').attr('data-value');
+		if(labels) parts.push("&labels=on");
+
+		var stroke = $('#stroke .active').attr('data-value');
+		if(stroke) parts.push("&stroke=" + stroke);
+
+		settings.xhr = function() {
+			var override = new XMLHttpRequest();
+			override.responseType = 'arraybuffer';
+			return override;
+		}
+	}
+
+	var method = $('#method .active').attr('data-value');
+	if(method == "upload") {
+		var file = $('#file').get(0).files && $('#file').get(0).files.item(0);
+		if(!file) return alert("Please select a file.");
+
+		getBase64fromFile(file).then(function(base64image) {
+			settings.url = parts.join("");
+			settings.data = base64image;
+
+			console.log(settings);
+			cb(settings);
+		});
+	} else {
+		var url = $('#url').val();
+		if(!url) return alert("Please enter an image URL");
+
+		parts.push("&image=" + encodeURIComponent(url));
+
+		settings.url = parts.join("");
+		console.log(settings);
+		cb(settings);
+	}
+};
+
+var getBase64fromFile = function(file) {
+	return new Promise(function(resolve, reject) {
+		var reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function() {
+			resolve(reader.result);
+		};
+		reader.onerror = function(error) {
+			reject(error);
+		};
+	});
+};
--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Config Base pytorch FrameWork
 
-This Framework  
+ARAS-Farabi Experimental Framework for Skill Assessment in Capsulorhexis Surgery
 
-Download AFCID Dataset:
-Contant mjahmadi@email.kntu.ac.ir
+Download AFCID Dataset and .pt Files:
+Contant arasfarabi.dataset@gmail.com
 
 Real-time performance in detecting and tracking the capsulorhexis cystotome and pupil using the AFCID dataset:
 [[Watch the video]](https://youtu.be/L6AiMw0MMPo)
+
+# Citation
+@INPROCEEDINGS{AFCID2021,
+  title           = "ARAS-Farabi Experimental Framework for Skill Assessment in Capsulorhexis Surgery",
+  booktitle       = "2021 9th International Conference on Robotics and
+                     Mechatronics ({ICRoM})",
+  author          = "Mohammad Javad Ahmadi, Mohammad Sina Allahkaram, etal.",
+  publisher       = "IEEE",
+  month           =  nov,
+  year            =  2021,
+  conference      = "2021 9th International Conference on Robotics and
+                     Mechatronics (ICRoM)",
+  location        = "Tehran, Iran"
+}
+
